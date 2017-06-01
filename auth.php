ba1a9ff17d18627ec39892fab0af99e88ed71a4d
triwidya <triwidyastuti767@gmail.com>
2017-06-02 06:26:54 +0700

    ui

--- auth.php
+++ auth.php
@@ -1,11 +1,10 @@
-<?php  ?>
 <head>
   <meta charset="UTF-8">
   <title>Calm breeze login screen</title>
-
-
-      <link rel="stylesheet" href="css/auth.css">
-
+  <script src="https://www.gstatic.com/firebasejs/4.1.1/firebase.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="js/auth.js"></script>
+  <link rel="stylesheet" href="css/auth.css">
 
 </head>
 
@@ -14,10 +13,10 @@
 	<div class="container">
 		<h1>Selamat Datang</h1>
 
-		<form class="form">
-			<input type="text" placeholder="Username">
-			<input type="password" placeholder="Password">
-			<button type="submit" id="login-button">Login</button>
+		<form class="form-group">
+			<input type="email" class="from-control" id="txtEmail"  placeholder="Email">
+			<input type="password" class="from-control" id="txtPassword" placeholder="Password">
+			<button type="submit" id="btnLogin">Login</button>
 		</form>
 	</div>
 
@@ -36,7 +35,7 @@
 </div>
   <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
 
-  <script src="assets/js/auth.js"></script>
-
+  <script src="js/auth.js"></script>
+2
 </body>
 <?php include 'foot.php'; ?>
