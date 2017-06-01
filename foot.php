f769abc71a7b8a609fa2d40f9cdbe849de3ddd27
Ari Fajrianda Alfi <real.rezz@gmail.com>
2017-06-02 05:06:48 +0700

    Sample Auth with Firebase

--- foot.php
+++
@@ -1,8 +0,0 @@
-      </main>
-    </div>
-  </body>
-  <footer>
-    <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
-    <script defer src="js/index.js"></script>
-  </footer>
-</html>
